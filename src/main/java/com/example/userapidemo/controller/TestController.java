547af725a50d4fdbdf4f6f5c8556efb10af77f05
XinhuiZhao0555 <71317312+XinhuiZhao0555@users.noreply.github.com>
2023-11-28 20:51:10 -0500

    Update TestController.java

--- src/main/java/com/example/userapidemo/controller/TestController.java
+++ src/main/java/com/example/userapidemo/controller/TestController.java
@@ -8,6 +8,6 @@ public class TestController {
 
     @GetMapping
     public String helloWorld() {
-        return "Welcome to the world of CI/CD!!! test CI";
+        return "Welcome to the world of CI/CD!!! test";
     }
 }
